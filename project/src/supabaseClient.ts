1ebcc776cedeea977f647275835ae681776f44b8
projectpolaris08 <barallasab@outlook.com>
2025-05-22 16:48:55 +0800

    added supabaseclient

--- project/src/supabaseClient.ts
+++ project/src/supabaseClient.ts
@@ -1,7 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://pfncgbzfsvxqgxyygldp.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmbmNnYnpmc3Z4cWd4eXlnbGRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4MjI3NTQsImV4cCI6MjA2MzM5ODc1NH0.NstSnyos7AnnE0beGFEQz0McKc0KOSgGKnnj6cKopZA";
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL!;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY!;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Missing Supabase environment variables");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
